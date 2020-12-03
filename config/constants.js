705190228638fe435e12c08bafa141266d6684c5
Dhruv Jain <dhruv.jain93@gmail.com>
2020-12-03 14:36:02 +0530

    fix: Fetch google auth credentials from env vars

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
-const google_client_id = '137444265231-6hv5i4kvbbqoh7ifasas2pqrno9ao67n.apps.googleusercontent.com';
-const google_client_secret = '4ufQNz2IYlBItXKmawbfUX59';
-const google_callback_url = 'http://localhost:3000/users/auth/google/callback';
+const google_client_id = process.env.GOOGLE_CLIENT_ID;
+const google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
+const google_callback_url = (process.env.DOMAIN || 'http://localhost:3000') + '/users/auth/google/callback';
 
 module.exports = {google_client_id, google_callback_url, google_client_secret};
