09cadb54667b151988ea9f3ae8a5bb3040cdd59c
Dhruv Jain <dhruv.jain93@gmail.com>
2020-12-05 18:58:12 +0530

    ftr: Use google-auth-library to verify if user logged in via google and if it is the same user or not and issue a new web token

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
+const loginController = require('./../controllers/login');
 
 router.use('/home', require('./home'));
 router.use('/users', require('./users'));
 router.use('/userFeed', require('./userFeed'));
 router.use('/userLocation', require('./userLocation'));
 router.use('/incidents',require('./incidents'));
+router.post('/login', loginController.createToken);
+
 module.exports = router;
