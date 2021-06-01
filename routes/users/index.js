e1a1b42bbd048c28dcdfd6d62b14175cc2e10b98
DESKTOP-KGAIMJL\Hp-user <nehra7991@gmail.com>
2021-06-01 16:18:53 +0530

    User profile post edit, post show

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,5 +5,7 @@ const passport = require('passport');
 const users_controller = require('./../../controllers/users/users_controller');
 
 router.get('/profile', passport.authenticate('jwt', {session: false}), users_controller.profile);
+router.get('/userPosts',  passport.authenticate('jwt', {session: false}), users_controller.userPosts);
+router.post('/updatePost',passport.authenticate('jwt', {session: false}), users_controller.updatePost);
 
 module.exports = router;
